3936934ab1a24fd218b9573a258d7791c24d2132
NITHISHKUMAR A <147365806+NithishKumar64@users.noreply.github.com>
2024-10-15 18:13:30 +0530

    Create coffee.java

---
+++ coffee.java
@@ -0,0 +1,55 @@
+package nun;
+
+import org.testng.annotations.Test;
+public class coffeee {
+	
+	@Test(priority=0)
+
+	public void heat()
+
+	{
+
+	System.out.println("place the pan ");
+
+	}
+
+	@Test(priority=1)
+
+	public void water()
+
+	{
+
+	System.out.println("add half glass of water ");
+
+	}
+
+	@Test(priority=5)
+
+	public void checkthe_temp()
+
+	{
+
+	System.out.println(" check the temperature of the coffee");
+
+	}
+
+	@Test(priority=2)
+
+	public void cofeepowder()
+
+	{
+
+	System.out.println(" add coffee powder");
+
+	}
+
+	@Test(priority=3)
+
+	public void Milk()
+	{
+		System.out.println("cofee is cool");
+}
+	
+	
+
+}
